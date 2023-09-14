21d5058ba6d8d515224441e8b4c967e131e1de6b
Wanne Wisse <wanne.wisse@gmail.com>
2023-09-14 13:54:34 +0200

    output

--- tree.py
+++ tree.py
@@ -83,15 +83,15 @@ class Node():
             
         #best feature to split with the split point
         col_index,best_split_point,min_gini_total,min_gini_p1,min_gini_p2 = min(min_gini_for_all_features, key=lambda x: x[2])
-        print(col_index,best_split_point,min_gini_total,min_gini_p1,min_gini_p2)
+        print(f"best feature column: {col_index}, best split value(smaller then): {best_split_point}, minimal gini total: {min_gini_total}, minimal gini part 1: {min_gini_p1}, minimal gini part 2: {min_gini_p2}")
         self.split_feature = col_index
         self.split_value = best_split_point
 
         #set indices of data for left and right of split
         left_indices = x_data.loc[x_data.iloc[:, col_index]<best_split_point].index
         right_indices = x_data.loc[x_data.iloc[:, col_index]>=best_split_point].index
-        print(left_indices)
-        print(right_indices)
+        print("indices to smaller: ", left_indices)
+        print("indices to larger or equal: ",right_indices)
        
         #create left and right node
         left_node = Node(left_indices)
@@ -109,7 +109,7 @@ class Node():
     
 #sample data
 data = {'X': [1, 2, 3,4,5],
-        #'Z': [1, 3, 8, 10,20,30],
+        #'Z': [1, 3, 8, 10,20],
         'Y': [0,1,0,0,1]}
 data = pd.DataFrame(data)
 
@@ -120,9 +120,9 @@ root = Node(x.index)
 tree = Tree(root)
 nodes_to_visit = [root]
 while True:
-    print(nodes_to_visit)
+    print("------------------")
+    print("nodes in queue:", nodes_to_visit)
     if len(nodes_to_visit) == 0:
-        print(1)
         break
     current_node = nodes_to_visit[0]
     left_node,right_node = current_node.split()
